d98dcb71ad40f091ac6f8b84166bc8fcd2b2e351
Hunter Lai <1hunterlai@gmail.com>
2023-11-13 15:03:23 -0700

    Initial Commit

---
+++ SweetTooth/SweetToothApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetToothApp.swift
+//  SweetTooth
+//
+//  Created by Hunter Lai on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SweetToothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
